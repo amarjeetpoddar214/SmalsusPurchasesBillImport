7175912a93a622e24a5d133d599b9e21fa79153e
Amarjeet Kumar <amarjeetpoddar214@gmail.com>
2025-10-10 11:39:54 +0530

    Updated App.tsx for Aligment

--- src/webparts/smalsusPurchases/components/App.tsx
+++ src/webparts/smalsusPurchases/components/App.tsx
@@ -221,7 +221,7 @@ const App: React.FC<AppProps> = ({ context }) => {
         onImportTransactions={handleImportClick}
         onExportData={handleExportData}
       />
-      <main className="p-4 sm:p-6 lg:p-8  mx-auto">
+      <main className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
           <div className="lg:col-span-2 space-y-8">
             <PurchaseList
