7e2d41444cc5b451f025172a960af822dc946dd8
Amarjeet Kumar <amarjeetpoddar214@gmail.com>
2025-10-14 16:55:07 +0530

    Page Alignment & Full Page CSS, Updated App.tsx and Style.css

--- src/webparts/smalsusPurchases/components/App.tsx
+++ src/webparts/smalsusPurchases/components/App.tsx
@@ -237,7 +237,7 @@ const App: React.FC<AppProps> = ({ context }) => {
         onImportTransactions={handleImportClick}
         onExportData={handleExportData}
       />
-      <main className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
+      <main className="w-full max-w-full mx-auto p-4 sm:p-6 lg:p-8">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
           <div className="lg:col-span-2 space-y-8">
             <PurchaseList
